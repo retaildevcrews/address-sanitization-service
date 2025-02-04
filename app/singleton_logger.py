14caa336448a6ba15fb2f00daf22bc4ea811f330
gortegaMS <70150605+gortegaMS@users.noreply.github.com>
2025-02-04 20:28:08 +0000

    Initial commit

---
+++ app/singleton_logger.py
@@ -0,0 +1,36 @@
+import logging
+import os
+from datetime import datetime
+
+class SingletonLogger:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(SingletonLogger, cls).__new__(cls)
+            cls._instance._initialize_logger()
+        return cls._instance
+
+    def _initialize_logger(self):
+        self.logger = logging.getLogger("app_logger") # __name__
+        self.logger.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+        # Create a file handler
+        log_dir = "./logs"
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+        log_file = os.path.join(log_dir, f"log_{datetime.now().strftime('%Y-%m-%d')}.log")
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setFormatter(formatter)
+
+        # Create a stream handler
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+
+        # Add handlers to the logger
+        self.logger.addHandler(file_handler)
+        self.logger.addHandler(stream_handler)
+
+    def get_logger(self):
+        return self.logger
