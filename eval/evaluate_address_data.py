b8ae90847893e87ce022e032c15e2279a83b79af
Michael Burch <me@michaelburch.net>
2025-02-10 21:51:13 +0000

    update deps

---
+++ eval/evaluate_address_data.py
@@ -0,0 +1,67 @@
+import argparse
+import pandas as pd
+import pathlib
+from address_evaluator import AddressEvaluator
+from azure.ai.evaluation import evaluate
+
+
+def run_evaluation(dataset_path, output_path):
+    # Create the evaluators
+    azure_maps_evaluator = AddressEvaluator(strategy="azure")
+    mapbox_evaluator = AddressEvaluator(strategy="mapbox")
+
+    evaluators = {
+            azure_maps_evaluator.name: azure_maps_evaluator,
+            mapbox_evaluator.name: mapbox_evaluator,
+    }
+    # Run the evaluation
+    result = evaluate(
+        evaluation_name="azure_and_mapbox",
+        evaluators=evaluators,
+        data=dataset_path,
+        output_path=output_path,
+    )
+
+    # print(result)
+    summary = summarize_result(result, evaluators)
+    print(summary)
+
+def summarize_result(result, evaluators):
+    """
+    Generate the evaluation result
+    """
+    rows = result["rows"]
+    df = pd.DataFrame(rows)
+    output = []
+    for row in rows:
+        result = {}
+        matches = []
+        result["input_address"] = row['inputs.address']
+        result["country_code"] = row['inputs.country_code']
+        for evaluator in evaluators.keys():
+            matches.append(row[f"outputs.{evaluator}.address"])
+            result[f"{evaluator}_confidenceScore"] = row[f"outputs.{evaluator}.address"]['confidenceScore']
+        matches.sort(key=lambda x: x['confidenceScore'], reverse=True)
+        result["best_match"] = matches[0]
+        output.append(result)
+
+
+    return output
+
+if __name__ == "__main__":
+    # Read the dataset path from the command line
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dataset_path",
+        help="full path to the dataset .jsonl file",
+        default=str(pathlib.Path.cwd()) + "/eval/data/peru.jsonl",
+        type=str,
+    )
+    parser.add_argument(
+        "--output_path",
+        help="full path to the output .json file",
+        default=str(pathlib.Path.cwd()) + "/eval/data/results.json",
+    )
+    args = parser.parse_args()
+
+    run_evaluation(args.dataset_path, args.output_path)
