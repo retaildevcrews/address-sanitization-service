f48fa7edd614cf83729ba5557bca14b3aca524d5
Joaquin Rodriguez <joaquin@outlook.com>
2025-02-19 09:15:02 -0600

    Swagger docs fixes (#12)
    * Swagger docs fixes
    
    * fixing response model errors
    
    * fix bug with incorrect response from libpostal
    
    ---------
    
    Co-authored-by: Jorge <jorge.luna@utexas.edu>

--- app/main.py
+++ app/main.py
@@ -1,16 +1,20 @@
 # app/main.py
-import json
 
 from datetime import datetime
-
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 
 from .parsers_and_expanders.libpostal import parse_address as libpostal_parse_address
 from .parsers_and_expanders.libpostal import expand_address as libpostal_expand_address
 from .parsers_and_expanders.llm import LLMEntityExtraction
 
 from .exceptions import GeocodingError
-from .schemas import AddressRequest, AddressResponse, Address
+from .schemas import (
+    AddressRequest,
+    AddressResponse,
+    ParseAddressResponse,
+    ExpandAddressResponse,
+    Address,
+)
 from .strategies import StrategyFactory
 from .exceptions import GeocodingError
 from .utils import batch_executor
@@ -37,25 +41,34 @@ def health_check():
     return {"status": "healthy", "version": app.version}
 
 
-@app.get("/api/v1/address/parse/libpostal")
-async def parse_address_libpostal(address: str):
+@app.get(
+    "/api/v1/address/parse/libpostal",
+    response_model=ParseAddressResponse,
+    tags=["Address"],
+)
+async def parse_address(
+    address: str = Query(
+        ...,
+        description="Free-form address string (e.g. '1 Microsoft Way, Redmond, WA 98052')",
+    )
+):
     """
-    Parse a free-form address into its components using libpostal
-
-    Parameters:
+    **Parse a free-form address** into its components using libpostal.
+    \n
     - **address**: Free-form address string (e.g., "1 Microsoft Way, Redmond, WA 98052")
     """
     try:
-        parsed = libpostal_parse_address(address)
-        parsed_dict = {component[1]: component[0] for component in parsed}
-        response = {"address": address, "parsed": parsed_dict}
+        response = libpostal_parse_address(address)
         return response
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@app.get("/api/v1/address/expand/libpostal")
-async def expand_address_libpostal(address: str):
+@app.get("/api/v1/address/expand/libpostal",
+         response_model=ExpandAddressResponse,
+         tags=["Address"])
+async def expand_address(address: str = Query(...,
+                                              description="Free-form address string (e.g. '1 Microsoft Way, Redmond, WA 98052')")):
     """
     Parse a free-form address into its components using libpostal
 
@@ -63,9 +76,7 @@ async def expand_address_libpostal(address: str):
     - **address**: Free-form address string (e.g., "1 Microsoft Way, Redmond, WA 98052")
     """
     try:
-        result = libpostal_expand_address(address)
-
-        response = {"address": address, "expanded_address": result}
+        response = libpostal_expand_address(address)
         return response
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
@@ -143,29 +154,24 @@ async def expand_address_llm_batch(addresses: List[Address]):
 @app.post("/api/v1/address/sanitize", response_model=AddressResponse, tags=["Address"])
 async def sanitize_address(payload: AddressRequest):
     """
-    Process an address using the specified geocoding strategy
-
-    Parameters:
+    **Process an address** using the specified geocoding strategy.
+    \n
     - **address**: Free-form address string (e.g., "1 Microsoft Way, Redmond, WA 98052")
     - **country_code**: ISO 3166-1 alpha-2 country code (e.g., "US")
     - **strategy**: Geocoding provider to use (azure_search, mapbox, etc.)
     - **use_libpostal**: Whether to sanitize the address using libpostal (default: True)
     """
     try:
-        print("Payload Address:", payload.address)
-
         # Check the use_libpostal flag from the payload
         if payload.use_libpostal:
-            sanitized_address = libpostal_expand_address(payload.address)
-            print("Sanitized Address (libpostal):", sanitized_address)
+            sanitized_address = libpostal_expand_address(payload.address)["expanded_address"]
         else:
             sanitized_address = payload.address
-            print("Skipping libpostal. Using raw address:", sanitized_address)
 
         # Get the requested strategy
         strategy = StrategyFactory.get_strategy(payload.strategy)
 
-        # Execute geocoding using the (possibly) sanitized address
+        # Execute geocoding
         address_results = strategy.geocode(
             address=sanitized_address, country_code=payload.country_code
         )
