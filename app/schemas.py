462805484f234292c66c8f13e8c3f2ce0e4c8321
Alexandre Vilcek <vilcek@gmail.com>
2025-03-10 16:50:18 -0400

    updated address schema

--- app/schemas.py
+++ app/schemas.py
@@ -130,6 +130,16 @@ class AddressPayload(BaseModel):
         example="US",
         description="ISO country code (2 or 3 character format)",
     )
+    country: str = Field(
+        default="",
+        example="United States",
+        description="Full name of the country",
+    )
+    countrySubdivision: str = Field(
+        default="",
+        example="WA",
+        description="State or province within the country",
+    )
     countrySecondarySubdivision: str = Field(
         default="",
         example="Travis",
